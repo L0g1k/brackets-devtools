73aff0c4a7329caf66927aae085ee56c46625969
Jason <jason@pixeljet.net>
2013-10-13 15:24:48 +1100

    Added an internal utility feature that will find out the id of the live development extension.
    This is necessary because if you are in development mode, then chrome will generate a new id for the extension and therefore we have no way of knowing what the id will be. This isn't an issue on the chrome store since the ids are static.

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-define(["remoteDebug/main", "chrome/main", "chrome/debug"] ,
+define(["remoteDebug/main", "chrome/main", "chrome/debug"],
     function (remoteDebug, extension, debug) {
     return {
         init: function() {
@@ -11,6 +11,44 @@ define(["remoteDebug/main", "chrome/main", "chrome/debug"] ,
             extension.on("disconnect", function(page) {
                 console.debug("Page has disconnected");
             });
+            this.broadcastId();
+        },
+
+        broadcastId: function() {
+            chrome.management.getAll(function(apps){
+                var quickfire = apps.filter(function(app){
+                    return isQuickfire(app);
+                });
+                if(quickfire) {
+                    sendId();
+                } else {
+                    chrome.management.onInstalled.addListener(function(app){
+                        if(isQuickfire(app)) {
+                            sendId();
+                        }
+                    })
+                }
+            });
+
+            function isQuickfire(app) {
+                return app.name == "Quickfire";
+            }
+
+            function sendId() {
+                console.debug('Sending extension id');
+                var ws = new WebSocket("ws://127.0.0.1:9876");
+                ws.onopen = function () {
+                    ws.send(JSON.stringify({
+                        extensionId: chrome.extension.getBackgroundPage().location.host
+                    }));
+                };
+                ws.onmessage = function(event) {
+                    var msg = JSON.parse(event.data);
+                    if(msg['extensionIdSet']) {
+                        console.debug('Extension id set');
+                    }
+                }
+            }
         }
     };
-});
+});;
